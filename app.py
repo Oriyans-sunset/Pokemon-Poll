6c5636ca3e349cd8f8dd4f846a7a145fe047d20e
Oriyans-sunset <83832376+Oriyans-sunset@users.noreply.github.com>
2022-07-10 20:00:22 +0530

    Added comments

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ db = SQLAlchemy(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+#setting up Voter database
 class Voters(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
@@ -21,6 +22,8 @@ class Voters(db.Model):
 
 @app.route("/sign_in", methods=["GET", "POST"])
 def sign_in():
+
+    #using global variables to provide access to the voting() func
     global name, email
     name = request.form.get("name")
     email = request.form.get("email")
@@ -32,8 +35,12 @@ def sign_in():
 
 @app.route("/voting", methods=["GET", "POST"])
 def voting():
+    """
+    Desc: Updates the database with information
+    Paramaters: N/A
+    """
+
     voted_for = request.form.get("option", False)
-    print(voted_for, "-------------------------------")
 
     new_voter = Voters(name=name, email=email, voted_for=voted_for)
     db.session.add(new_voter)
@@ -43,19 +50,23 @@ def voting():
 
 @app.route("/update/<string:type>")
 def update(type):
+    """
+    Desc: Updates the image for that particular pokemon card
+    Paramaters: type("fire", "water", "grass")
+    """
+
+    #adding "type" parameter to base url fetches list of all pokemon of that type
     base_url = "https://pokeapi.co/api/v2/type/"
     required_url = base_url + type
 
-    #getting pokemon url
+    #getting pokemon url for a specific pokemon
     pokemon_url_response = requests.get(required_url)
     poke_url_dict = random.choice(pokemon_url_response.json()["pokemon"])
     poke_url = poke_url_dict["pokemon"]["url"]
-    print("POKE URL "+poke_url)
 
-    #getting pokemon image url reponse
+    #getting pokemon image url using pokemon url acquired above 
     pokemon_image_response = requests.get(poke_url)
     poke_img = pokemon_image_response.json()["sprites"]["versions"]["generation-vii"]["ultra-sun-ultra-moon"]["front_default"]
-    print(poke_img)
 
     poke_fire_img, poke_water_img, poke_grass_img = "", "", ""
     if type == "fire":
@@ -69,11 +80,14 @@ def update(type):
 
 @app.route("/result")
 def result():
+    """
+    Desc: Displays summary statistics for the poll
+    Paramaters: N/A
+    """
+
     fire_votes = len(db.session.query(Voters).filter(Voters.voted_for == "fire").all())
     water_votes = len(db.session.query(Voters).filter(Voters.voted_for == "water").all())
     grass_votes = len(db.session.query(Voters).filter(Voters.voted_for == "grass").all())
-
-    print(fire_votes, water_votes, grass_votes)
 
     return render_template("result.html", fire_votes=fire_votes, water_votes=water_votes, grass_votes=grass_votes)
 
